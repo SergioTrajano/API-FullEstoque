7afe1c1728f4726596cb78dc6535befb08584242
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 14:18:58 -0300

    create signIn route to login users

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { userController } from "../controllers/authController";
 import validateSchema from "../middlewares/schemaValidationMiddleware";
-import { signUpSchema } from "../schemas/userSchemas";
+import { signUpSchema, signInSchema } from "../schemas/userSchemas";
 
 const router = Router();
 
 router.post("/signUp", validateSchema(signUpSchema), userController.signUp);
+router.post("/signIn", validateSchema(signInSchema), userController.signIn);
 
 export default router;
