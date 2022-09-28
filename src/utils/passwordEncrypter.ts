fae93dfbccd28b6918ee45d66c02b8191714d3e7
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 13:36:32 -0300

    feat: add a password encrypter

---
+++ src/utils/passwordEncrypter.ts
@@ -0,0 +1,11 @@
+import { compareSync, hashSync } from "bcrypt";
+
+const SALT: number = Number(process.env.BCRYPT_SALT);
+
+export function encryptPassword(password: string) {
+	return hashSync(password, SALT);
+}
+
+export function comparePassword(password: string, encryptPassword: string) {
+	return compareSync(password, encryptPassword);
+}
