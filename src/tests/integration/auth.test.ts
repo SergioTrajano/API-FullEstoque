fdafe076a8ddad898ff70006ff8e981fb8f03275
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 13:57:10 -0300

    test: add more integration test to route signUp

--- src/tests/integration/auth.test.ts
+++ src/tests/integration/auth.test.ts
@@ -6,6 +6,14 @@ import client from "../../dbStrategy/postgres";
 
 const app = supertest(server);
 
+beforeEach(async () => {
+	await client.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
+});
+
+afterAll(async () => {
+	await client.$disconnect();
+});
+
 describe("test authRoutes", () => {
 	it("return 201 and a object that cotanins userData", async () => {
 		const newUser = factory.createUser();
@@ -15,5 +23,22 @@ describe("test authRoutes", () => {
 		expect(result.status).toBe(201);
 		expect(result.body).toBeInstanceOf(Object);
 		expect(await client.user.findUnique({ where: { id: result.body.id } })).not.toBeNull();
+	});
+
+	it("returns 409 for email in use", async () => {
+		const newUser = factory.createUser();
+
+		await app.post("/signUp").send(newUser);
+		const result = await app.post("/signUp").send(newUser);
+
+		expect(result.status).toBe(409);
+	});
+
+	it("returns 422 for invalid body", async () => {
+		const newUser = {};
+
+		const result = await app.post("/signUp").send(newUser);
+
+		expect(result.status).toBe(422);
 	});
 });
