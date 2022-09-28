a4f7d3c21ef585f89d8e93ce358443bd25aa7a75
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 13:41:14 -0300

    chore: add jest configs

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	coveragePathIgnorePatterns: [
+		"node_modules",
+		"test-config",
+		"interfaces",
+		"repositories",
+		"jestGlobalMocks.ts",
+		"<rootDir>/src/server.ts",
+		"<rootDir>/src/database.ts",
+		"<rootDir>/src/utils",
+		"<rootDir>/src/config",
+		"<rootDir>/tests/factory.ts",
+	],
+};
