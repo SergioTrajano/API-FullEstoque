38d0bdfafd715ad1a14cbcf3248008518632233c
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 12:17:23 -0300

    feat: add initial types of errors

---
+++ src/utils/errorTypes.ts
@@ -0,0 +1,22 @@
+function unathorized(entity: string) {
+	return { code: 401, message: `Invalid ${entity}.` };
+}
+
+function notFound(entity: string) {
+	return { code: 404, message: `${entity} not found.` };
+}
+
+function conflict(entity: string) {
+	return { code: 409, message: `${entity} already in use.` };
+}
+
+function unprocessableEntityError(error: { details: { message: String }[] }) {
+	return { code: 422, message: error.details.map((detail) => detail.message) };
+}
+
+export const errorType = {
+	unathorized,
+	notFound,
+	conflict,
+	unprocessableEntityError,
+};
