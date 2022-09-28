933dd84c99fb01313009a23a7b9730b9e5eb26f9
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 14:22:24 -0300

    test: update factory to when creating user insert them in db if passed param is true

--- tests/factory.ts
+++ tests/factory.ts
@@ -1,11 +1,16 @@
 import { faker } from "@faker-js/faker";
+import client from "../src/dbStrategy/postgres";
 
-function createUser() {
+async function createUser(bool: boolean) {
 	const user = {
 		email: faker.internet.email(),
 		password: faker.internet.password(10) + "@1At",
 		name: faker.animal.bird(),
 	};
+
+	if (bool) {
+		await client.user.create({ data: user });
+	}
 
 	return user;
 }
