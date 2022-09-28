b8661b958f7b0223e314a037ed41e86697d240e2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 13:13:15 -0300

    feat: integrate schemaValidator and signUP schema in signUp route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { userController } from "../controllers/authController";
+import validateSchema from "../middlewares/schemaValidationMiddleware";
+import { signUpSchema } from "../schemas/userSchemas";
 
 const router = Router();
 
-router.post("/signUp", userController.signUp);
+router.post("/signUp", validateSchema(signUpSchema), userController.signUp);
 
 export default router;
