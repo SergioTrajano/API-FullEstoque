cbb8a105441dc89e8ed076639865d34aef216899
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 12:13:27 -0300

    feat: add authRouter

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import "express-async-errors";
 
+import routes from "./routes/index";
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+server.use(routes);
 
 export default server;
