14c83ff0a042478133b93d107f6a495c8547a8b5
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 13:40:19 -0300

    fix: encrypt user password before adding in db

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,13 @@
 import { createUser, userRepository } from "../repositories/userRepository";
 import { errorType } from "../utils/errorTypes";
+import { encryptPassword } from "../utils/passwordEncrypter";
 
 async function insertUser(newUserData: createUser) {
 	if (await userRepository.getByEmail(newUserData.email)) {
 		throw errorType.conflict("Email");
 	}
 
-	const userData = await userRepository.create(newUserData);
+	const userData = await userRepository.create({ ...newUserData, password: encryptPassword(newUserData.password) });
 
 	return userData;
 }
