45c63b974aa51e0083b699fc9993d785762746ad
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 13:05:32 -0300

    feat: add a schema validator

---
+++ src/middlewares/schemaValidationMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { ObjectSchema } from "joi";
+
+import { errorType } from "../utils/errorTypes";
+
+export default function validateSchema(schema: ObjectSchema) {
+	(req: Request, res: Response, next: NextFunction) => {
+		const data = req.body;
+
+		const { error } = schema.validate(data, { abortEarly: false });
+
+		if (error) {
+			throw errorType.unprocessableEntityError(error);
+		}
+
+		next();
+	};
+}
