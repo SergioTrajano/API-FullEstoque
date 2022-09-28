ae6125e78e3a7541adc6683a0d3c2dd6961caf01
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 12:38:33 -0300

    feat: add users repository

---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,24 @@
+import { User } from "@prisma/client";
+import client from "../dbStrategy/postgres";
+
+type createUser = Omit<User, "id">;
+
+async function create(newUserData: createUser) {
+	const userData: User = await client.user.create({ data: newUserData });
+
+	return userData;
+}
+
+async function deleteUser(id: number) {
+	await client.user.delete({ where: { id } });
+}
+
+async function changePassword(id: number, newPassword: string) {
+	await client.user.update({ data: { password: newPassword }, where: { id } });
+}
+
+export const userRepository = {
+	create,
+	deleteUser,
+	changePassword,
+};
