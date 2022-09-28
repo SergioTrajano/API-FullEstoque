ee453a0005f614614f89a024e96805190200970b
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-28 12:54:04 -0300

    feat: add a route to create users

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { userController } from "../controllers/authController";
+
 const router = Router();
+
+router.post("/signUp", userController.signUp);
 
 export default router;
