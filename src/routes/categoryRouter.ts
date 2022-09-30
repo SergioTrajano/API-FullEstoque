a48941e81225ac82cf1fa938f5e066a343d0b6f7
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-30 20:43:15 -0300

    feat: add route to get user's categories

--- src/routes/categoryRouter.ts
+++ src/routes/categoryRouter.ts
@@ -7,7 +7,11 @@ import validateSchema from "../middlewares/schemaValidationMiddleware";
 const router = Router();
 
 router.post("/categories", validateSchema(createCategorySchema), categoryController.create);
+
 router.put("/categories/:categoryId", validateSchema(createCategorySchema), categoryController.update);
+
 router.delete("/categories/:categoryId", categoryController.remove);
+
+router.get("/categories", categoryController.find);
 
 export default router;
