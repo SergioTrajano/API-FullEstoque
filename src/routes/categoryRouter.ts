468baf2f0d885129249bf7c1a4327c99604005b4
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-30 20:09:10 -0300

    feat: add route to delete category

--- src/routes/categoryRouter.ts
+++ src/routes/categoryRouter.ts
@@ -8,5 +8,6 @@ const router = Router();
 
 router.post("/categories", validateSchema(createCategorySchema), categoryController.create);
 router.put("/categories/:categoryId", validateSchema(createCategorySchema), categoryController.update);
+router.delete("/categories/:categoryId", categoryController.remove);
 
 export default router;
