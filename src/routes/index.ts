1a87d372e53058ee502f3a7c239a58a22a025f6e
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-30 19:40:58 -0300

    feat: integrate headerMIddleware and category router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
 import authRouter from "./authRouter";
+import categoryRouter from "./categoryRouter";
+
+import validateHeader from "../middlewares/validateHeaderMiddleware";
 
 const router = Router();
 
 router.use(authRouter);
+
+router.use(validateHeader);
+
+router.use(categoryRouter);
 
 export default router;
