7bdd10163fdd5ba3575f3fd34c515cf02a950052
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-30 19:28:57 -0300

    feat: create schema to header for user send bearer token

---
+++ src/schemas/headerSchema.ts
@@ -0,0 +1,12 @@
+import joi from "joi";
+
+const headerSchema = joi
+	.object({
+		authorization: joi
+			.string()
+			.pattern(/^Bearer /)
+			.required(),
+	})
+	.options({ allowUnknown: true });
+
+export default headerSchema;
