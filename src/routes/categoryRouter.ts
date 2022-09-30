1a0f5b43d50af8c1fd2109274c76b98099d17b9e
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-30 20:01:03 -0300

    feat: add route to update categorie

--- src/routes/categoryRouter.ts
+++ src/routes/categoryRouter.ts
@@ -3,10 +3,10 @@ import { Router } from "express";
 import { categoryController } from "../controllers/categoryController";
 import { createCategorySchema } from "../schemas/categorySchemas";
 import validateSchema from "../middlewares/schemaValidationMiddleware";
-import validateHeader from "../middlewares/validateHeaderMiddleware";
 
 const router = Router();
 
 router.post("/categories", validateSchema(createCategorySchema), categoryController.create);
+router.put("/categories/:categoryId", validateSchema(createCategorySchema), categoryController.update);
 
 export default router;
