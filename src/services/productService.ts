d3fb68f072f814c2ac485afefddfad10295e3c69
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-10-01 15:51:59 -0300

    feat: add service to update product

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -25,6 +25,35 @@ async function create(userId: number, newProductData: createProduct) {
 	return newProduct;
 }
 
+async function update(userId: number, updateProductData: createProduct, productId: number) {
+	await userService.getById(userId);
+	const dbCategory = await categoryService.findById(updateProductData.categoryId);
+	const dbManufacturer = await manufacturerService.findById(updateProductData.manufacturerId);
+	const dbProductByBarCode = await productRepository.findByBarcode(updateProductData.barcode, userId);
+	const dbProductById = await productRepository.findById(productId);
+
+	if (dbCategory.userId !== userId) {
+		throw errorType.forbbiden("Invalid category");
+	}
+	if (dbManufacturer.userId !== userId) {
+		throw errorType.forbbiden("Invalid manufacturer");
+	}
+	if (dbProductByBarCode && dbProductByBarCode.id !== productId) {
+		throw errorType.conflict("Barcode");
+	}
+	if (!dbProductById) {
+		throw errorType.notFound("Product");
+	}
+	if (dbProductById.userId !== userId) {
+		throw errorType.forbbiden();
+	}
+
+	const updatedProduct = await productRepository.update(updateProductData, productId);
+
+	return updatedProduct;
+}
+
 export const productService = {
 	create,
+	update,
 };
