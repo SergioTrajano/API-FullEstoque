f7b526867c3a706829bb76dd38d70a9a9ab4c0c6
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-10-01 18:05:44 -0300

    feat: add a service to update client data

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -17,6 +17,27 @@ async function create(newClientData: createClient, userId: number) {
 	return newClient;
 }
 
+async function update(updateClient: createClient, userId: number, clientId: number) {
+	await userService.getById(userId);
+	const dbClientById = await clientRepository.getById(clientId);
+	const dbClientByCPF = await clientRepository.getByCPF(updateClient.CPF, userId);
+
+	if (!dbClientById) {
+		throw errorType.notFound("Client");
+	}
+	if (dbClientById.userId !== userId) {
+		throw errorType.forbbiden();
+	}
+	if (dbClientByCPF && dbClientByCPF.id !== clientId) {
+		throw errorType.conflict("CPF");
+	}
+
+	const updatedClient = await clientRepository.update(updateClient, clientId);
+
+	return updatedClient;
+}
+
 export const clientService = {
 	create,
+	update,
 };
