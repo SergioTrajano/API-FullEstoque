d1c3a7b72b9fe142e84f3a0d0387b9fa681bdc06
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-10-02 15:41:55 -0300

    feat: add controller to update purchase data

--- src/controllers/purchaseController.ts
+++ src/controllers/purchaseController.ts
@@ -12,6 +12,17 @@ async function create(req: Request, res: Response) {
 	res.status(201).send(newPurchase);
 }
 
+async function update(req: Request, res: Response) {
+	const userId = res.locals.userId;
+	const dataToUpdate: createPurchase = req.body;
+	const purchaseId: number = Number(req.params.purchaseId);
+
+	const updatedPurchase = await purchaseService.update(dataToUpdate, userId, purchaseId);
+
+	res.status(200).send(updatedPurchase);
+}
+
 export const purchaseController = {
 	create,
+	update,
 };
