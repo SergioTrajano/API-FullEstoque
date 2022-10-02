4904824e2284f9d65a73c391e3cee0c9b803a6fc
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-10-02 16:07:24 -0300

    feat: add controller to get user's purchases

--- src/controllers/purchaseController.ts
+++ src/controllers/purchaseController.ts
@@ -31,8 +31,17 @@ async function remove(req: Request, res: Response) {
 	res.sendStatus(200);
 }
 
+async function find(req: Request, res: Response) {
+	const userId = res.locals.userId;
+
+	const userPurchases = await purchaseService.find(userId);
+
+	res.status(200).send(userPurchases);
+}
+
 export const purchaseController = {
 	create,
 	update,
 	remove,
+	find,
 };
