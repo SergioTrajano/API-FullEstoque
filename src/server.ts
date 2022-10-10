7b55febb9b4c1c3a00aaa1a420a673553d06ccd5
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-10-10 17:04:15 -0300

    test: add condition to access test router

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,17 @@ import "express-async-errors";
 
 import routes from "./routes/index";
 import errorHandler from "./middlewares/errorHandler";
+import testRouter from "./routes/testRouter";
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+if (process.env.NODE_ENV === "test") {
+	server.use(testRouter);
+}
+
 server.use(routes);
 server.use(errorHandler);
 
